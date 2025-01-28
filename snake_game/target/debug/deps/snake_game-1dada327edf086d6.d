ab3ca4355730ffbbcac259cad1bc6366edd029ba
Fabian <fabian.luque@shiplux.com>
2025-01-27 21:55:46 -0300

    enums and match

--- snake_game/target/debug/deps/snake_game-1dada327edf086d6.d
+++
@@ -1,5 +0,0 @@
-/Users/kzelly/Workspace/Study/rust/snake_game/target/debug/deps/snake_game-1dada327edf086d6: main.rs
-
-/Users/kzelly/Workspace/Study/rust/snake_game/target/debug/deps/snake_game-1dada327edf086d6.d: main.rs
-
-main.rs:
--- snake_game/target/debug/snake_game.d
+++
@@ -1,1 +0,0 @@
-/Users/kzelly/Workspace/Study/rust/snake_game/target/debug/snake_game: /Users/kzelly/Workspace/Study/rust/snake_game/main.rs
